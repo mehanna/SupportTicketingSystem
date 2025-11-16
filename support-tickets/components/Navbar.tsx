b26e7528b032a8e8773953b437548b12cf4ce2ce
Maged <magedehanna@gmail.com>
2025-11-16 16:14:05 -0600

    Show the login state in the nav bar

--- support-tickets/components/Navbar.tsx
+++ support-tickets/components/Navbar.tsx
@@ -1,15 +1,39 @@
 import Link from 'next/link';
+import { getCurrentUser } from '@/lib/current-user';
 
 const Navbar = async () => {
+  const user = await getCurrentUser();
 
   return (
-    <nav className='bg-white border-b border-gray-200 shadow-md px-6 py-4 flex justify-between items-center'>
+    <nav className='bg-white border-b border-gray-200 px-6 py-4 flex justify-between items-center'>
       <div>
-        <Link href='/' className='text-2xl font-bold text-blue-600 hover:text-blue-700 transition'>
+        <Link href='/' className='text-xl font-bold text-blue-600'>
           QuickTicket
         </Link>
       </div>
       <div className='flex items-center space-x-4'>
+        {user ? (
+          <>
+            <Link
+              href='/tickets/new'
+              className='text-blue-600 hover:text-blue-900 font-medium transition'
+            >
+              New Ticket
+            </Link>
+            <Link
+              href='/tickets'
+              className='text-blue-600 hover:text-blue-900 font-medium transition'
+            >
+              My Tickets
+            </Link>
+            <Link
+              href='/logout'
+              className='bg-blue-600 text-white px-6 py-2 rounded shadow hover:bg-blue-700 transition'
+            >
+              Logout
+            </Link>
+          </>
+        ) : (
           <>
             <Link
               href='/login'
@@ -24,7 +48,7 @@ const Navbar = async () => {
               Register
             </Link>
           </>
-
+        )}
       </div>
     </nav>
   );
