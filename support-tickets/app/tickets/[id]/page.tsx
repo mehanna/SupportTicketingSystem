4ca6d350c556ec02dbb8ad7e1f691b0518a5dba3
Maged <magedehanna@gmail.com>
2025-11-16 13:45:28 -0600

    Get the id the (TicketDetailsPage)

--- support-tickets/app/tickets/[id]/page.tsx
+++ support-tickets/app/tickets/[id]/page.tsx
@@ -1,8 +1,17 @@
 import {getTicketById} from '@/actions/ticket.actions';
+import { logEvent } from '@/utils/sentry';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { getPriorityClass } from '@/utils/ui';
 
 
-const TicketDetailsPage = () => {
-  return <div>Ticket Details Page</div>;
+const TicketDetailsPage = async (props: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await props.params;
+
+
+  return (<div>Ticket Details Page {id}</div>);
 };
 
 export default TicketDetailsPage;
